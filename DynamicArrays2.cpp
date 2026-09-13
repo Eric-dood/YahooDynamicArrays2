543f277afae24969777cbfff946ecd5eaf22f7fe
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-13 15:12:03 -0700

    Added comments and finalized the lab

--- DynamicArrays2.cpp
+++ DynamicArrays2.cpp
@@ -13,23 +13,38 @@ void displayArray(string*);
 //Start of main()
 int main()
 {
+    //Add the name list that's compromised of 5 different names
     string names[SIZE] = { "Eric", "Joey", "Zoe", "Sarah", "Holly" };
+    //Add a dynamic string array that will contain the 5 names altogether
     string *nameList = new string[SIZE];
     nameList = names;
 
+    //Display the original array by using displayArray()
+    cout << "Original array: ";
+    displayArray(nameList);
+
+    //Reverse the nameList array by using reverseArray()
     reverseArray(nameList);
+    //Then display the modified nameList array by using displayArray()
+    cout << endl << "Reversed array: ";
     displayArray(nameList);
 }
 
 //Define reverseArray()
 void reverseArray(string *str)
 {
+    //Initialize a temporary string variable that will be used for looping
     string temp;
+    //Use a for-loop to go through all elements
     for (int i = 0; i < SIZE / 2; i++)
     {
+        //Initialize the 'last' variable which will be used for the last elements of the array
         int last = SIZE - (i+1);
+        //Initialize the temporary variable that will contain the last, unmodified element of the array
         temp = *(str + last);
+        //Make the last element of the variable be the first or ith element of the variable
         *(str + last) = *(str + i);
+        //Set the ith element as the temporary variable
         *(str + i) = temp;
     }
 }
