67aba2069698a6d967d205dd662fe89cca014e07
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-13 11:01:43 -0700

    Initial commit for Lab 7

---
+++ DynamicArrays2.cpp
@@ -0,0 +1,17 @@
+//COMSC-210 | Lab 6 | Eric-Giulio Hedes
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Define the global constant variable that will be used for the array size.
+const int SIZE = 5;
+
+//Define the function prototypes
+void reverseArray(string*);
+void displayArray(string*);
+
+//Start of main()
+int main()
+{
+    string *names = new string[SIZE];
+}
